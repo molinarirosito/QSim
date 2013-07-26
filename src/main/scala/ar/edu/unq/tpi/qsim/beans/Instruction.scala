b6331e32d6132ef96f153542551307c3bd3afd15
susanrosito <rosito.susana@gmail.com>
2013-07-26 16:51:00 -0300

    Modificando la creacion de las clases

--- src/main/scala/ar/edu/unq/tpi/qsim/beans/Instruction.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/beans/Instruction.scala
@@ -1,28 +1,16 @@
 package ar.edu.unq.tpi.qsim.beans
 
-class Instruction(){
+class Instruction(var operationCode: Int, var name: String, var op1: String, var op2: String){
 
-  var operationCode: Int =_
-  var name: String =_
-  var op1: String =_
-  var op2: String =_
-  
-  
-
-override def toString() = 
-     name + " " + op1 + " " + op2
+override def toString() =  name + " " + op1 + " " + op2
   
 }
 
-case class MUL(operand1: String, operand2: String) extends Instruction
+case class MUL(operand1: String, operand2: String) extends Instruction(0000,"MUL",operand1,operand2)
 {
 
-  operationCode = 0000
-  name = "MUL"
-  op1 = operand1
-  op2 = operand2
-  
 }
+
 object Test extends App{
   var v = Integer.parseInt("0F",16)
   print(v)
