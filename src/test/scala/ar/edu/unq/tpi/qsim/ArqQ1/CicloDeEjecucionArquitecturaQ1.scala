d385d5b321752ac298f0b7fefb83a4f2b55d10e8
susanrosito <rosito.susana@gmail.com>
2013-09-07 18:01:30 -0300

    Incorpore la dependencia ScalaTest

---
+++ src/test/scala/ar/edu/unq/tpi/qsim/ArqQ1/CicloDeEjecucionArquitecturaQ1.scala
@@ -0,0 +1,22 @@
+package ar.edu.unq.tpi.qsim.ArqQ1
+
+import org.scalatest._
+import scala.collection.mutable.Stack
+
+class CicloDeEjecucionArquitecturaQ1  extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[Int]
+    a [NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
+}
