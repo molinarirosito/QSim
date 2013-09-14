2142518a6629b3a33b08e50b44d64c3a673d6583
susanrosito <rosito.susana@gmail.com>
2013-09-14 02:25:25 -0300

    incorpore que el parser tome el 0x sin que tire error.

--- src/main/scala/ar/edu/unq/tpi/qsim/parser/ArchitecturesQParser.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/parser/ArchitecturesQParser.scala
@@ -1,73 +1,75 @@
 package ar.edu.unq.tpi.qsim.parser
 
-import scala.util.parsing.combinator.syntactical.StdTokenParsers
-import scala.util.parsing.combinator.ImplicitConversions
-import scala.util.parsing.combinator.token.StdTokens
+import scala.util.matching.Regex
+import scala.util.parsing.combinator._
+import scala.util.parsing.combinator._
 import scala.util.parsing.combinator.lexical.StdLexical
+import scala.util.parsing.combinator.syntactical.StdTokenParsers
+import scala.util.parsing.combinator.token.StdTokens
+import scala.util.parsing.input._
 import ar.edu.unq.tpi.qsim.model._
 import scala.collection.mutable.ArrayBuffer
-import ar.edu.unq.tpi.qsim.model.Programa
 
-trait ArchitecturesQParser extends StdTokenParsers  with ImplicitConversions {
+trait ArchitecturesQParser extends JavaTokenParsers with ImplicitConversions {
   type Tokens = StdTokens
   val lexical = new StdLexical
   lexical.reserved ++= List("MOV", "SUB", "ADD", "DIV", "MUL", "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7")
-  lexical.delimiters ++= List(",", "[",  "]","0x")
-  
-  def registers = "R0" ^^^ R0 |    
-  				  "R1" ^^^ R1 | 
-  				  "R2" ^^^ R2 |
-  				  "R3" ^^^ R3 | 
-  				  "R4" ^^^ R4 |
-  				  "R5" ^^^ R5 | 
-  				  "R6" ^^^ R6 | 
-  				  "R7" ^^^ R7
-  
-  def register = registers 
-  
-  def inmediate = "0x" ~> numericLit ^^ {case direction => Inmediato(new W16(direction))}
-  
-  def direct = "[" ~> inmediate <~ "]" ^^ {case direction => Directo(direction)}
-  
+  lexical.delimiters ++= List(",", "[", "]", "0x")
+
+  def registers = "R0" ^^^ R0 |
+    "R1" ^^^ R1 |
+    "R2" ^^^ R2 |
+    "R3" ^^^ R3 |
+    "R4" ^^^ R4 |
+    "R5" ^^^ R5 |
+    "R6" ^^^ R6 |
+    "R7" ^^^ R7
+
+  def register = registers
+
+  def inmediate = "0x" ~> "[0-9A-Z]+".r ^^ { case direction ⇒ Inmediato(new W16(direction)) }
+
+
+  def direct = "[" ~> inmediate <~ "]" ^^ { case direction ⇒ Directo(direction) }
+
   //def directionIndirect = "[" ~>directionDirect  <~ "]" ^^ {case direction => DirectionIndirect(direction)}
-  
-  def directionable = register | inmediate | direct 
-  
-  def asignable = register | direct
+
+  def directionable = register | inmediate //| direct 
+
+  def asignable = register //| direct
 
   //operaciones
-  def instruccions2 = "MOV" | "SUB" | "DIV" | "ADD" | "MUL" 
-  
+  def instruccions2 = "MOV" | "SUB" | "DIV" | "ADD" | "MUL"
+
   def instruccions1 = "CALL"
-    
+
   def instruccions0 = "RET"
-    
 
-  def instruction2 = instruccions2 ~ asignable ~ ("," ~> directionable ) ^^
-    { case ins ~ dir1 ~ dir2 => Class.forName(s"ar.edu.unq.tpi.qsim.model.$ins").getConstructor(classOf[ModoDireccionamiento],classOf[ModoDireccionamiento]).newInstance(dir1, dir2).asInstanceOf[Instruccion_DosOperandos] }
+  def instruction2 = instruccions2 ~ asignable ~ ("," ~> directionable) ^^
+    { case ins ~ dir1 ~ dir2 ⇒ Class.forName(s"ar.edu.unq.tpi.qsim.model.$ins").getConstructor(classOf[ModoDireccionamiento], classOf[ModoDireccionamiento]).newInstance(dir1, dir2).asInstanceOf[Instruccion_DosOperandos] }
 
-  def instruction1 = instruccions1 ~ asignable  ^^
-    { case ins ~ dir1 => Class.forName(s"ar.edu.unq.tpi.qsim.model.$ins").getConstructor(classOf[ModoDireccionamiento]).newInstance(dir1).asInstanceOf[Instruccion_UnOperando] }
-  
+  def instruction1 = instruccions1 ~ asignable ^^
+    { case ins ~ dir1 ⇒ Class.forName(s"ar.edu.unq.tpi.qsim.model.$ins").getConstructor(classOf[ModoDireccionamiento]).newInstance(dir1).asInstanceOf[Instruccion_UnOperando] }
+
   def instruction0 = instruccions0 ^^
-    { case ins => Class.forName(s"ar.edu.unq.tpi.qsim.model.$ins").getConstructor().newInstance().asInstanceOf[Instruccion_SinOperandos] }
-  
-  def instructions = instruction2 //instruction0 | instruction1 | 
-  
-  def program = rep(instructions) ^^ {case instructions => Programa(ArrayBuffer()++instructions)}
+    { case ins ⇒ Class.forName(s"ar.edu.unq.tpi.qsim.model.$ins").getConstructor().newInstance().asInstanceOf[Instruccion_SinOperandos] }
 
-  def parse(input: String) = phrase(program)(new lexical.Scanner(input))
+  def instructions = instruction2 //instruction0 | instruction1 | 
+
+  def program = rep(instructions) ^^ { case instructions ⇒ Programa(ArrayBuffer() ++ instructions) }
+
+  def parse(input: String) = parseAll(program, input)
 }
 
 object QuarqExample extends App with ArchitecturesQParser {
 
   val theCode = """
-		SUB R2, 0x0023  
+		SUB R0, 0x023  
     """
 
   parse(theCode) match {
-    case Success(result, _) => println(result)
-    case Failure(msg, i) => println("[Failure] " + s" $msg in $i")
-    case Error(msg, i) => println("[Error] " + s" $msg in $i")
+    case Success(result, _) ⇒ println(result)
+    case Failure(msg, i) ⇒ println("[Failure] " + s" $msg in $i")
+    case Error(msg, i) ⇒ println("[Error] " + s" $msg in $i")
   }
 }
