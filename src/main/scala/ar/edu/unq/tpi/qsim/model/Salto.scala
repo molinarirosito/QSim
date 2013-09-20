2a38b2b85d481a5306068baa057a516c7730d89e
Tatiana <tatianamolinari90@gmail.com>
2013-09-19 22:40:16 -0300

    Comentarios en la clase Salto

--- src/main/scala/ar/edu/unq/tpi/qsim/model/Salto.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/model/Salto.scala
@@ -2,8 +2,23 @@ package ar.edu.unq.tpi.qsim.model
 import ar.edu.unq.tpi.qsim.utils._
 
 class Salto(var salto: Int) {
+  
+  /**
+   * Devuelve el numero que tiene el salto en String
+   * @return String
+   */
 	override def toString() =  value.toString
+	
+ /**
+   * Devuelve la representacion binaria del numero que tiene el salto
+   * @return String
+   */
 	def toBinary: String = Util.toBinary8B(salto)
+ 
+ /**
+   * Devuelve el numero que tiene el salto
+   * @return Int
+   */	
 	def value : Int = salto
 	
 	def this(binary_chain: String) {
