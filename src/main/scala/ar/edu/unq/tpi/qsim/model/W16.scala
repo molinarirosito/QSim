fa457868de8a9bde22cefae665e89d58ee05e3f2
Tatiana <tatianamolinari90@gmail.com>
2013-09-19 22:59:33 -0300

    Comentarios en la clase W16

--- src/main/scala/ar/edu/unq/tpi/qsim/model/W16.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/model/W16.scala
@@ -4,42 +4,111 @@ import ar.edu.unq.tpi.qsim.utils.Util
 
 class W16(var hex: String) {
   
-  override def toString() =  hex
-  def equals(w16:W16): Boolean =  this.value == w16.value
-  def value : Int = Util.toInteger(hex)
-  def toBinary: String = Util.hexToBinary(hex)
-  def ++ = hex = Util.toHex4(this.value + 1)
-  def ++(salto:Int) = hex= Util.toHex4(this.value + salto)
-  def -- = hex= Util.toHex4(this.value -1)
-  
-  
-	def this(value: Int) {
-    this(Util.toHex4(value));
-
-  }
-  
   var signo: Int = 0
   
+  /**
+   * Devuelve el valor hexadecimal que contiene
+   * @return String
+   */
+  override def toString() =  hex
+  
+  /**
+   * Devuelve un boleano indicando si el W16 receptor de este mensaje
+   * es igual al W16 enviado por parametro comparando sus valores.
+   * @parameters w16:W16
+   * @return Boolean
+   */
+  def equals(w16:W16): Boolean =  this.value == w16.value
+  
+  /**
+   * Devuelve el valor entero del hexadecimal que contiene.
+   * @return Int
+   */
+  def value : Int = Util.toInteger(hex)
+  
+   /**
+   * Devuelve la representacion en binario del hexadecimal que contiene.
+   * @return Int
+   */
+  def toBinary: String = Util.hexToBinary(hex)
+  
+  /**
+   * Aumenta el hexadecimal que contiene en uno.
+   * 
+   */
+  def ++ = hex = Util.toHex4(this.value + 1)
+  
+ /**
+   * Aumenta el hexadecimal que contiene en el valor 
+   * que contenga el salto pasado por parametro.
+   * @parameters salto:Int
+   */
+  def ++(salto:Int) = hex= Util.toHex4(this.value + salto)
+  
+ /**
+   * Decrementa el hexadecimal que contiene en uno.
+   * 
+   */
+  def -- = hex= Util.toHex4(this.value -1)
+  
+ /**
+   * Cambia valor hexadecimal que contiene por la representacion  
+   * del valor hexadecimal del entero pasado por parametro.
+   * @parameters value: Int
+   */ 
+  def this(value: Int) =   this(Util.toHex4(value))
+  
+  /**
+   * Devuelve un nuevo W16 con el valor anterio mas el valor del 
+   * salto recibido.
+   * @parameters salto: Int 
+   * @return W16
+   */
   def ss(salto:Int) : W16 = new W16(Util.toHex4(this.value + salto))
 
+ /**
+   * Le asigna un nuevo valor hexadecimal recibido por parametro.
+   * @parameters w16 : String 
+   */
   def :=(w16 : String) = hex = w16
   
+ /**
+   * Devuelve el resultado de la suma entre los dos W16.
+   * @parameters w16 : W16
+   * @return W16
+   */
   def +(w16 : W16) :W16 = {
    val result_value = Util.toHex(this.value+w16.value)
    new W16(result_value)
   }
+  
+  /**
+   * Devuelve el resultado de la resta entre los dos W16.
+   * @parameters w16 : W16
+   * @return W16
+   */
   def -(w16 : W16) :W16 = {
     
    val result_value = Util.toHex(this.value-w16.value)
    new W16(result_value)
   }
   
+ /**
+   * Devuelve el resultado de la multiplicacion entre los dos W16.
+   * @parameters w16 : W16
+   * @return W16
+   */
   def *(w16 : W16) :W16 = {
     
    val result_value = Util.toHex(this.value*w16.value)
    new W16(result_value)
   }
   
+ /**
+   * Devuelve el resultado de la division entre los dos W16.
+   * @parameters w16 : W16
+   * @return W16
+   */
   def /(w16 : W16) :W16 = {
     
    val result_value = Util.toHex(this.value/w16.value)
