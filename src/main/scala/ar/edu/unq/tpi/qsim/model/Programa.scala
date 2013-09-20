a3bf343f52a964358bcdfbbb774fe3198cd38dab
Tatiana <tatianamolinari90@gmail.com>
2013-09-19 22:37:13 -0300

    Comentarios en la clase Programa

--- src/main/scala/ar/edu/unq/tpi/qsim/model/Programa.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/model/Programa.scala
@@ -5,6 +5,12 @@ import scala.collection.mutable.Map
 
 object Programa {
 
+  /**
+   * Aplica los datos pasados por una lista de que contiene opcciones e instrucciones 
+   * relacionadas y devuelve un programa.
+   * @parameters datas: List[(Option[String], Instruccion)]
+   * @return Programa
+   */
   def apply(datas: List[(Option[String], Instruccion)]):Programa = {
     var etiquetas = Map[String, Instruccion]()
     datas.foreach(entry => {
@@ -12,32 +18,50 @@ object Programa {
         case (Some(etiqueta), i) => etiquetas(etiqueta) = i
          case _ =>
       }
-
     })
     new Programa(datas.map(_._2), etiquetas)
   }
 }
 
 class Programa(var instrucciones: List[Instruccion], var etiquetas: Map[String, Instruccion] = Map()) {
-
   var indice = 0
-
+  
+  /**
+   * Devuelve el tamaño del programa
+   * @return Int
+   */
   def tamanioDelPrograma(): Int = (instrucciones.map(i => i.tamanioHex).fold(0)(_ + _)) / 4
 
+  /**
+   * Actualiza el indice, aumentandolo en uno 
+   * 
+   */
   def actualizarIndice() {
     indice = indice + 1
   }
 
+  /**
+   * Obtiene la siguiente instruccion segun lo indique el indice y lo aumenta.
+   * @return Instruccion
+   */
   def obtenerInstruccion(): Instruccion = {
     var siguienteInstruccion = instrucciones(indice)
     actualizarIndice()
     siguienteInstruccion
   }
 
+  /**
+   * Retorna un booleano verificando si el programa termino.
+   * @return Boolean
+   */
   def finalizo(): Boolean = {
     indice == (instrucciones.length)
   }
   
+  /**
+   * Devuelve el String que representa el programa de una manera mas amigable.
+   * @return String
+   */
   override def toString() = {
     s"Program(instrucciones: $instrucciones, etiquetas: $etiquetas)"
   }
