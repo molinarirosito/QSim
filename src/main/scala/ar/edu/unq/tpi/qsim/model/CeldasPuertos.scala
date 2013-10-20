589bdff758eeb011e417dab632a886b9a2c046b8
Tatiana <tatianamolinari90@gmail.com>
2013-10-20 18:33:09 -0300

    ventana de puertos, faltan mas cosas igual

--- src/main/scala/ar/edu/unq/tpi/qsim/model/CeldasPuertos.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/model/CeldasPuertos.scala
@@ -118,6 +118,6 @@ import ar.edu.unq.tpi.qsim.exeptions._
    * Cambia el estado de una celda reservada para puertos por el pasado por parametro.
    * @param Int, Int
    */
-  def setStateCelda(num_celda: Int,state: CeldaState.Type) = celdas(num_celda).state = state
+  def setStateCelda(num_celda: Int,state: CeldaState.Type) = celdas(num_celda - 65520).state = state
 
 }
