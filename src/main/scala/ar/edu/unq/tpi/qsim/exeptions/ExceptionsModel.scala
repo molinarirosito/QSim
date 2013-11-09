b366981b9b3c582f4d82db26b854f8245d466261
Tatiana <tatianamolinari90@gmail.com>
2013-11-09 19:15:48 -0300

    Cambios en las exeptions para que sean capturadas por la interfaz

--- src/main/scala/ar/edu/unq/tpi/qsim/exeptions/ExceptionsModel.scala
+++ src/main/scala/ar/edu/unq/tpi/qsim/exeptions/ExceptionsModel.scala
@@ -1,19 +1,20 @@
 package ar.edu.unq.tpi.qsim.exeptions
+import org.uqbar.commons.model.UserException
 
 import scala.util.control.Exception
 
-case class CodigoInvalidoException(smth:String) extends Exception(smth) {
+case class CodigoInvalidoException(smth:String) extends UserException(smth) {
 
 }
 
-case class CeldaFueraDeMemoriaException(smth:String) extends Exception(smth) {
+case class CeldaFueraDeMemoriaException(smth:String) extends UserException(smth) {
 
 }
 
-case class CeldaFueraDePuertosException(smth:String) extends Exception(smth) {
+case class CeldaFueraDePuertosException(smth:String) extends UserException(smth) {
 
 }
 
-case class SyntaxErrorException(smth:String) extends Exception(smth) {
+case class SyntaxErrorException(smth:String) extends UserException(smth) {
 
 }
